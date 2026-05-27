4ccb3458e0891f7900b7a7a749497706aed3e38f
KarlLbs <karl.lubbos@ensea.fr>
2026-05-27 09:51:12 +0200

    Refactor Encoder block

--- src/unet_diffusion_model/models/encoder.py
+++ src/unet_diffusion_model/models/encoder.py
@@ -5,28 +5,28 @@
 import torch.nn as nn
 
 
-class EncoderBlock(nn.Module):
+class EncoderSubBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3)
-        self.activation = nn.SiLU()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3),
+            nn.SiLU(),
+            nn.Conv2d(out_channels, out_channels, 3),
+            nn.SiLU(),
+        )
 
     def forward(self, input):
-        x = self.conv1(input)
-        x = self.activation(x)
-        x = self.conv2(x)
-        return self.activation(x)
+        return self.block(input)
 
 
 class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.maxpool = nn.MaxPool2d(2)
-        self.block1 = EncoderBlock(3, 64)
-        self.block2 = EncoderBlock(64, 128)
-        self.block3 = EncoderBlock(128, 256)
-        self.block4 = EncoderBlock(256, 512)
+        self.block1 = EncoderSubBlock(3, 64)
+        self.block2 = EncoderSubBlock(64, 128)
+        self.block3 = EncoderSubBlock(128, 256)
+        self.block4 = EncoderSubBlock(256, 512)
 
     def forward(self, input):
         x1 = self.block1(input)
